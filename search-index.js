4a3b7e9ecb96e2ff1881c2a75292fe54bcfbaf1b
louisabricot <louisabricot@users.noreply.github.com>
2023-03-28 14:45:09 +0000

    deploy: cc5767a0c76ec38c6aa481de48f406695730f4b6

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"gbmu":{"doc":"","t":[5],"n":["main"],"q":["gbmu"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+"gbmu":{"doc":"","t":"F","n":["main"],"q":[[0,"gbmu"]],"d":[""],"i":[0],"f":[[[]]],"c":[],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
