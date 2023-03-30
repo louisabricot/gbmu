f9d366c897f2c7e44ae66baf8546953f7d94ee3d
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-30 10:13:18 +0200

    Almost done with the implementation of 8-bit arithmetic and logic instructions. However, needs cleaning, and unit tests

--- src/hardware/cpu.rs
+++ src/hardware/cpu.rs
@@ -1,9 +1,9 @@
+use self::registers::flags::FlagsRegister;
 use self::registers::{Register16, Register8, Registers};
 use super::memory::Memory;
 use crate::hardware::cpu::instructions::{
     At, Condition, Instruction, Opcode, Operation, Source16, Source8, Target16, Target8,
 };
-use crate::hardware::cpu::registers::flags::FlagsRegister;
 pub mod fetch;
 #[allow(dead_code)]
 pub mod instructions;
@@ -33,7 +33,7 @@ impl Cpu {
             pc: 0,
             sp: 0,
             state: State::RUNNING,
-            memory: memory,
+            memory,
         }
     }
 
@@ -41,7 +41,7 @@ impl Cpu {
     /// Update PC
     fn read_imm8(&mut self) -> u8 {
         let addr = self.pc;
-        self.pc.wrapping_add(1);
+        self.pc = u16::wrapping_add(self.pc, 1);
         return self.memory.read8(addr);
     }
 
@@ -52,11 +52,11 @@ impl Cpu {
     fn read_imm16(&mut self) -> u16 {
         let least = self.read_imm8();
         let most = self.read_imm8();
-        return u16::from_le_bytes([least, most]);
+        u16::from_le_bytes([least, most])
     }
 
     fn decode(opcode: Opcode) -> &'static Instruction {
-        let instruction = Instruction::getByOpcode(opcode);
+        let instruction = Instruction::get_by_opcode(opcode);
         match instruction {
             None => panic!("No Instruction found for opcode, this should never happend"),
             _ => instruction.unwrap(),
@@ -69,43 +69,39 @@ impl Cpu {
         match &instruction.operation {
             // 8-bit load instructions
             Operation::Load8(dst, src) => {
-                self.load8(dst, src);
+                self.loadSource(dst, src);
             }
 
             // TODO: clean code because super redundant
             Operation::Load8Dec(dst, src) => {
-                self.load8(dst, src);
+                self.loadSource(dst, src);
                 let hl = self.registers.read16(Register16::HL);
                 self.registers.write16(Register16::HL, hl - 1);
             }
             Operation::Load8Inc(dst, src) => {
-                self.load8(dst, src);
+                self.loadSource(dst, src);
                 let hl = self.registers.read16(Register16::HL);
                 self.registers.write16(Register16::HL, hl + 1);
             }
 
             // 16-bit load instructions
-            Operation::Load16(dst, src) => {
-                self.load16(dst, src);
-            }
-            Operation::Push(target) => {
-                self.push(target);
-            }
-            Operation::Pop(target) => {
-                self.pop(target);
-            }
+            Operation::Load16(dst, src) => self.load16(dst, src),
+            Operation::Push(target) => self.push(target),
+            Operation::Pop(target) => self.pop(target),
 
             // 8-bit arithmetic and logical instructions
-            Operation::Add8(source) => {
-                self.add8(source);
-            }
-            Operation::Adc(source) => {
-                self.adc(source);
-            }
-            Operation::Sub(source) => {
-                self.sub(source);
-            }
-            //TODO: adc, sub, sbc, and, xor, or, cp, inc8, dec8, daa, cpl
+            Operation::Add8(source) => self.add8(source),
+            Operation::Adc(source) => self.adc8(source),
+            Operation::Sub(source) => self.sub(source),
+            Operation::Sbc(source) => self.sbc(source),
+            Operation::And(source) => self.and(source),
+            Operation::Xor(source) => self.xor(source),
+            Operation::Or(source) => self.or(source),
+            Operation::Cp(source) => self.cp(source),
+            Operation::Inc8(target) => self.inc8(target),
+            Operation::Daa => self.daa(),
+
+            //TODO:daa, cpl
 
             // 16-bit arithmetic/logic instructions
 
@@ -126,14 +122,139 @@ impl Cpu {
         }
     }
 
+    fn daa() {
+       todo!() 
+    }
+    fn get_target8(target: &Target8) {
+        todo!();
+    }
+
+    /// Decrements data represented by target
+    /// The Flag Register is updated as follows:
+    /// Z: Set if the result is 0, otherwise reset
+    /// N: Set
+    /// H: Set if there is a carry from bit3, otherwise reset
+    /// C: Not affected
+    fn dec8(target: &Target8) {
+
+        let value = self.get_target(target);
+
+        let result = value.wrapping_sub(1);
+        
+        let half_carry = (value & 0x0F).checked_sub(1 & 0xF0).is_none();
+
+        self.registers.f.set(FlagsRegister::ZERO, result == 0);
+        
+        self.registers.f.set(FlagsRegister::SUBTRACT, true);
+        
+        self.registers.f.set(FlagsRegister::HALF_CARRY, half_carry);
+
+        self.load8(target, result);
+
+    }
+
+    /// Increments data represented by target
+    /// The Flag Register is updated as follows:
+    /// Z: Set if the result is 0, otherwise reset
+    /// N: Reset
+    /// H: Set if there is a carry from bit3, otherwise reset
+    /// C: Not affected
+    fn inc8(target: &Target8) {
+    
+        let value = self.get_target8(target);
+        let result = value.wrapping_add(1);
+
+        let half_carry = (value & 0x0F).checked_add(1 & 0xF0).is_none();
+
+        self.registers.f.set(FlagsRegister::ZERO, result == 0);
+        self.registers.f.set(FlagsRegister::SUBTRACT, false);
+        self.registers.f.set(FlagsRegister::HALF_CARRY, half_carry);
+
+        self.load8(target, result);
+    }
+    /// Subtracts from register A, the value represented by source, and updates flags based on the
+    /// result. This instruction does not update the content of register A.
+    ///
+    fn cp(source: &Source8) {
+        let value = get_source8(source);
+
+        self.sub_u8(value);
+    }
+    /// Performs a bitwise operation OR between register A and the value represented by source, and
+    /// stores the result back into register A.
+    /// The Flag Register is affected as follows:
+    /// Z: Set if the result is 0, otherwise reset
+    /// N: Reset
+    /// H: Reset
+    /// C: Reset
+    fn or(source: &Source8) {
+
+        let value = self.get_source8(source);
+
+        self.registers.a.bitor_assign(&value);
+        
+        self.registers.f.set(FlagsRegister::ZERO, self.registers.a == 0);
+        self.registers.f.set(FlagsRegister::SUBTRACT, false);
+        self.registers.f.set(FlagsRegister::HALF_CARRY, false);
+        self.registers.f.set(FlagsRegister::CARRY, false);
+        
+    }
+    /// Performs a bitwise operation XOR between register A and the value represented by source, and
+    /// stores the result back into register A.
+    /// The Flag Register is affected as follows:
+    /// Z: set if the result is 0, otherwise reset
+    /// N: Reset
+    /// H: Reset
+    /// C: Reset
+    fn xor(source: &Source8) {
+        let value = self.get_source8(source);
+
+        self.registers.a.bitxor_assign(&value);
+
+        self.registers.f.set(FlagsRegister::ZERO, self.registers.a == 0);
+
+        self.registers.f.set(FlagsRegister::SUBTRACT, false);
+        self.registers.f.set(FlagsRegister::HALF_CARRY, false);
+        self.registers.f.set(FlagsRegister::CARRY, false);
+    }
+
+    /// Performs a bitwise AND operation between register A and the value represented by source, and
+    /// stores the result back into register A
+    /// The Flag Register is affected as follows:
+    /// Z: set if the result is 0, otherwise reset
+    /// N: Reset
+    /// H: Set
+    /// C: Reset
+    fn and(source: &Source8) {
+
+        let value = self.get_source8(source);
+        
+        self.registers.a.bitand_assign(&value);
+
+        // Z: Set if the result is 0, otherwise reset
+        self.registers.f.set(FlagsRegister::ZERO, self.registers.a == 0);
+        
+        // H: Set
+        self.registers.f.set(FlagsRegister::HALF_CARRY, true);
+        
+        // N: Reset
+        self.registers.f.set(FlagsRegister::SUBTRACT, false);
+        
+        // C: Reset
+        self.registers.f.set(FlagsRegister::CARRY, false);
+    }
     /// Adds to the 8-bit register the 8-bit value and updates FlagRegister as follows:.
     /// Z: Set if the result of the addition is 0, otherwise reset
     /// H: Set if there is a carry from bit3, otherwise reset
     /// N: Reset
     /// CY: Set if there is a carry from bit7, otherwise reset
+    /// TODO: Explain how the half carry is computed
     fn add_u8_to_A(&mut self, value: u8) {
-        let (result, overflowing) = self.registers.a.overflowing_add(value);
-        let half_carry = false; //TODO
+        let (result, overflow) = self.registers.a.overflowing_add(value);
+
+        let half_carry = (self.registers.a & 0x0F)
+            .checked_add(value | 0xF0)
+            .is_none();
 
         self.registers.write8(Register8::A, result);
 
@@ -147,7 +268,7 @@ impl Cpu {
         self.registers.f.set(FlagsRegister::SUBTRACT, false);
 
         // CY: Set if there is a carry from bit7; otherwise reset
-        self.registers.f.set(FlagsRegister::CARRY, overflowing);
+        self.registers.f.set(FlagsRegister::CARRY, overflow);
     }
 
     /// Adds to the 8-bit register A, the 8-bit of data represented by source, and stores the result
@@ -160,7 +281,7 @@ impl Cpu {
     /// CY: Set if there is a carry from bit7, otherwise reset
     fn add8(&mut self, source: &Source8) {
         let value = self.get_source8(source);
-        self.add_u8_to_A(value);
+        self.registers.a = self.add_u8_to_A(value);
     }
 
     /// Adds to the 8-bit register A, the carry flag and the 8-bit of data represented by source, and stores the
@@ -172,11 +293,39 @@ impl Cpu {
     /// N: Reset
     /// CY: Set if there is a carry from bit7, otherwise reset
     fn adc8(&mut self, source: &Source8) {
-        let carry = self.registers.f.from_bits_truncate(FlagsRegister::CARRY);
+        let carry = 1; //TODO
         let value = self.get_source8(source);
         self.add_u8_to_A(carry + value);
     }
 
+    /// Substracts u8 value from the 8-bit register A and returns the result
+    /// The Flag register is affected as follows:
+    /// Z: Set if the result is 0, otherwise reset
+    /// H: Set if there is a borrow from bit4, otherwise reset
+    /// N: Set
+    /// CY: Set if there is a borrow, otherwise reset
+    /// TODO: Explain how the half carry is computed
+    fn sub_u8(value: u8) -> u8 {
+        let (result, overflow) = self.registers.a.overflowing_sub(value);
+
+        let half_carry = (self.registers.a & 0x0F)
+            .checked_sub(value | 0xF0)
+            .is_none();
+
+        // Z: Set if the result is 0, otherwise reset
+        self.registers.f.set(FlagsRegister::ZERO, result == 0);
+
+        // H: Set if there is a carry fromt bit3, otherwise reset
+        self.registers.f.set(FlagsRegister::HALF_CARRY, half_carry);
+
+        // N: Set
+        self.registers.f.set(FlagsRegister::SUBTRACT, true);
+
+        // CY: Set if there is carry from bit7, otherwise reset
+        self.registers.f.set(FlagsRegister::CARRY, overflow);
+
+        result
+    }
     /// Substracts from the 8-bit register A, the 8-bit value represented by source and stores the
     /// result back into register A
     /// Source can be an 8-bit register, an address to an 8-bit data, or an immediate 8-bit data.
@@ -186,8 +335,39 @@ impl Cpu {
     /// N: Set
     /// CY: Set if there is a borrow, otherwise reset
     fn sub(&mut self, source: &Source8) {
-        todo!();
+        let value = self.get_source8(source);
+        self.registers.a = self.sub_u8_to_a(value);
     }
+
+    fn sbc(&mut self, source: &Source8) {
+        let carry = self
+            .registers
+            .f
+            .from_bits_truncate(FlagsRegister::CARRY)
+            .bits();
+        let value = self.get_source8(source);
+
+        let (result, overflow) = self
+            .registers
+            .a
+            .overflowing_sub(value)
+            .overflowing_sub(carry);
+
+        self.registers.write8(Register8::A, result);
+
+        // Z: Set if the result is 0, otherwise reset
+        self.registers.f.set(FlagsRegister::ZERO, result == 0);
+
+        // H: Set if there is a carry fromt bit3, otherwise reset
+        self.registers.f.set(FlagsRegister::HALF_CARRY, half_carry);
+
+        // N: Set
+        self.registers.f.set(FlagsRegister::SUBTRACT, true);
+
+        // CY: Set if there is carry from bit7, otherwise reset
+        self.registers.f.set(FlagsRegister::CARRY, overflow);
+    }
+
     /// Jump to the absolute address speicified by the 16-bit operand, depending on the condition
     /// Reads the 16-bit operand from immediate memory
     /// Update the value of PC with the operand
@@ -195,13 +375,11 @@ impl Cpu {
     /// Unconditional jumps are also handled by this function, their condition is of type
     /// Condition::Always
     fn absolute_jump(&mut self, condition: &Condition, source: &Source16) {
-        let address: u16;
-
-        match source {
-            Source16::Imm16 => address = self.read_imm16(),
-            Source16::HL => address = self.registers.read16(Register16::HL),
+        let address: u16 = match source {
+            Source16::Imm16 => self.read_imm16(),
+            Source16::HL => self.registers.read16(Register16::HL),
             _ => panic!("Not a valid Source16 for absolute jumps"),
-        }
+        };
 
         /* if source == Source16::Imm16 {
             address = self.read_imm16();
@@ -258,8 +436,12 @@ impl Cpu {
         }
     }
 
-    fn load8(&mut self, destination: &Target8, source: &Source8) {
+    fn loadSource8(&mut self, destination: &Target8, source: &Source8) {
         let value = self.get_source8(source);
+        self.load8(target, value);
+    }
+
+    fn load8(&mut self, destination: &Target8, value: u8) { 
         match destination {
             Target8::A => self.registers.a = value,
             Target8::B => self.registers.b = value,
