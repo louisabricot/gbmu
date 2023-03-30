ff5a84c1e1f698740d2c9d0bfa04d9ef16b4ee48
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-30 15:24:10 +0200

    shorten name of struct

--- src/hardware/cpu/registers/flags.rs
+++ src/hardware/cpu/registers/flags.rs
@@ -5,23 +5,23 @@ bitflags! {
     /// Consists of 4 flags that are set and reset according to the results of instruction
     /// execution
     #[derive(Debug)]
-    pub struct FlagsRegister: u8 {
+    pub struct Flags: u8 {
 
         /// Set to 1 when the result of an operation is 0; otherwise reset
-        const ZERO = 0b1000_0000;
+        const Z = 0b1000_0000;
 
         /// Set to 1 following execution of the subtraction instruction, regardless of the result
-        const SUBTRACT = 0b0100_0000;
+        const N = 0b0100_0000;
 
         /// Set to 1 when an operation results in carrying from or borrowing to bit 3
-        const HALF_CARRY = 0b0010_0000;
+        const H = 0b0010_0000;
 
         /// Set to 1 when an operation results in carrying from or borrowing to bit 7
-        const CARRY = 0b0001_0000;
+        const C = 0b0001_0000;
     }
 }
 
-impl FlagsRegister {
+impl Flags {
     /// Returns true when the flag status matches the condition, otherwise returns false
     /// Special case: Condition::Always always returns true.
     /// The relation between conditions and flags are as follows:
@@ -34,10 +34,10 @@ impl FlagsRegister {
     ///
     pub fn check_condition(&self, condition: &Condition) -> bool {
         match condition {
-            Condition::NC => !self.contains(FlagsRegister::CARRY),
-            Condition::NZ => !self.contains(FlagsRegister::ZERO),
-            Condition::Z => self.contains(FlagsRegister::ZERO),
-            Condition::C => self.contains(FlagsRegister::CARRY),
+            Condition::NC => !self.contains(Flags::C),
+            Condition::NZ => !self.contains(Flags::Z),
+            Condition::Z => self.contains(Flags::Z),
+            Condition::C => self.contains(Flags::C),
             Condition::Always => true,
         }
     }
