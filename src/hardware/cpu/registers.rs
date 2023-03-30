d29aa1cc7f2b6335086ef4ab3cc2d227aa8b6141
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-30 15:24:38 +0200

    removed unused functions

--- src/hardware/cpu/registers.rs
+++ src/hardware/cpu/registers.rs
@@ -1,4 +1,4 @@
-use crate::hardware::cpu::registers::flags::FlagsRegister;
+use crate::hardware::cpu::registers::flags::Flags;
 
 pub mod flags;
 
@@ -9,25 +9,13 @@ pub mod flags;
 #[derive(Debug)]
 pub struct Registers {
     pub a: u8,
-    pub f: FlagsRegister,
+    pub f: Flags,
     pub b: u8,
     pub c: u8,
     pub d: u8,
     pub e: u8,
     pub h: u8,
     pub l: u8,
-}
-
-#[derive(Debug)]
-pub enum Register8 {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    H,
-    L,
 }
 
 #[derive(Debug, Clone)]
@@ -47,7 +35,7 @@ impl Registers {
             c: 0,
             d: 0,
             e: 0,
-            f: FlagsRegister::empty(),
+            f: Flags::empty(),
             h: 0,
             l: 0,
         }
@@ -65,24 +53,6 @@ impl Registers {
         }
     }
 
-    pub fn msb(&self, r16: Register16) -> u8 {
-        match r16 {
-            Register16::AF => self.a,
-            Register16::BC => self.b,
-            Register16::DE => self.d,
-            Register16::HL => self.h,
-        }
-    }
-
-    pub fn lsb(&self, r16: Register16) -> u8 {
-        match r16 {
-            Register16::AF => self.f.bits(),
-            Register16::BC => self.c,
-            Register16::DE => self.e,
-            Register16::HL => self.l,
-        }
-    }
-
     /// Writes the u16 num into the pair of registers represented by Register16
     /// The first register of the pair stores the most significant byte of the num, and the
     /// second register stores the least significant byte
@@ -90,7 +60,7 @@ impl Registers {
         match r16 {
             Register16::AF => {
                 self.a = (num >> u8::BITS) as u8;
-                self.f = FlagsRegister::from_bits_truncate(num as u8);
+                self.f = Flags::from_bits_truncate(num as u8);
             }
             Register16::BC => {
                 self.b = (num >> u8::BITS) as u8;
@@ -104,34 +74,6 @@ impl Registers {
                 self.h = (num >> u8::BITS) as u8;
                 self.l = num as u8;
             }
-        }
-    }
-
-    /// Returns the content stored in the register represented by r8
-    pub fn read8(&self, r8: Register8) -> u8 {
-        match r8 {
-            Register8::A => self.a,
-            Register8::B => self.b,
-            Register8::C => self.c,
-            Register8::D => self.d,
-            Register8::E => self.e,
-            Register8::F => self.f.bits() as u8,
-            Register8::H => self.h,
-            Register8::L => self.l,
-        }
-    }
-
-    /// Writes to the register represented by r8, the u8 num
-    pub fn write8(&mut self, r8: Register8, num: u8) {
-        match r8 {
-            Register8::A => self.a = num,
-            Register8::B => self.b = num,
-            Register8::C => self.c = num,
-            Register8::D => self.d = num,
-            Register8::E => self.e = num,
-            Register8::F => self.f = FlagsRegister::from_bits_truncate(num),
-            Register8::H => self.h = num,
-            Register8::L => self.l = num,
         }
     }
 }
