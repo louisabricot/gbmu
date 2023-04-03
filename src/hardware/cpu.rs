cb1982a9eae7bdb90e416955088f19563f97f0f2
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-04-03 16:20:48 +0200

    Added srl isntruction, test and doc

--- src/hardware/cpu.rs
+++ src/hardware/cpu.rs
@@ -109,7 +109,7 @@ impl Cpu {
 
             // Rotate, shift and bit operations
 
-            //TODO: Sla, Swap, Sra, Srl
+            //TODO: Swap, Srl
             Operation::Rlca => self.rlca(),
             Operation::Rla => self.rla(),
             Operation::Rrca => self.rrca(),
@@ -119,6 +119,7 @@ impl Cpu {
             Operation::Rr(target) => self.rr(target),
             Operation::Sla(target) => self.sla(target),
             Operation::Sra(target) => self.sra(target),
+            Operation::Srl(target) => self.srl(target),
             //TODO: bit, set, res
             // Control Flow instruction
             //TODO: Ccf, Scf, Nop, Halt, Stop, Di, Ei, Jp, Jr, Call, Ret, Reti, Rst
@@ -132,6 +133,26 @@ impl Cpu {
         }
     }
 
+    /// Rotates the content of *target* to the right.  
+    /// Copies bit0 to the `carry` flag and resets bit7.  
+    /// `Flag Register` is updated as follows:  
+    /// `Z`: Set if the result is 0, otherwise reset  
+    /// `H`: Reset  
+    /// `N`: Reset  
+    /// `C`: Set if bit0 is 1 before the rotation, otherwise reset  
+    fn srl(&mut self, target: Operand8) {
+        let value = self.get_operand8(target);
+        let bit0 = value & 1;
+        let new_value = value >> 1;
+
+        self.load_u8(target, new_value);
+
+        self.registers.f.set(Flags::C, bit0 == 1);
+        self.registers.f.set(Flags::Z, new_value == 0);
+        self.registers.f.set(Flags::H, false);
+        self.registers.f.set(Flags::N, false);
+    }
+    
     /// Rotates the content of *target* to the right.  
     /// Copies bit0 to the `carry` flag and preserves bit7.  
     /// `Flag Register` is updated as follows:  
@@ -2065,6 +2086,40 @@ mod tests {
         cpu.sra(Operand8::Addr(At::HL));
         assert_eq!(cpu.memory.read16(3), 0x00);
         assert!(cpu.registers.f.contains(Flags::Z));
+        assert!(!cpu.registers.f.contains(Flags::N));
+        assert!(!cpu.registers.f.contains(Flags::H));
+        assert!(cpu.registers.f.contains(Flags::C));
+    }
+    
+    #[test]
+    fn test_srl() {
+        let mut cpu = Cpu {
+            registers: Registers {
+                a: 0x01,
+                b: 0x85,
+                c: 3,
+                d: 0x8A,
+                e: 16,
+                f: Flags::empty(),
+                h: 0,
+                l: 3,
+                sp: 0xFFF8,
+                pc: 0,
+            },
+            state: State::Running,
+            memory: Memory::new(vec![2, 255, 147, 255, 0, 38, 23, 3, 34, 213, 99, 43, 13]),
+        };
+
+        cpu.srl(Operand8::A);
+        assert_eq!(cpu.registers.a, 0x00);
+        assert!(cpu.registers.f.contains(Flags::Z));
+        assert!(!cpu.registers.f.contains(Flags::N));
+        assert!(!cpu.registers.f.contains(Flags::H));
+        assert!(cpu.registers.f.contains(Flags::C));
+        
+        cpu.srl(Operand8::Addr(At::HL));
+        assert_eq!(cpu.memory.read16(3), 0x7F);
+        assert!(!cpu.registers.f.contains(Flags::Z));
         assert!(!cpu.registers.f.contains(Flags::N));
         assert!(!cpu.registers.f.contains(Flags::H));
         assert!(cpu.registers.f.contains(Flags::C));
