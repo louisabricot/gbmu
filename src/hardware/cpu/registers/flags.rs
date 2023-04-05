28db5f9736ab8e8a9e16b51ea167e9b758ca4f66
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-04-05 13:18:16 +0200

    Added tests to the flag register

--- src/hardware/cpu/registers/flags.rs
+++ src/hardware/cpu/registers/flags.rs
@@ -9,21 +9,16 @@
 //! - **Z** : The *Zero* flag.  
 //! Set to 1 when the result of an operation is 0; otherwise reset.  
 //!
-//! 
 //! - **N** : The *Subtract* flag.  
 //! Set to 1 after a subtraction instruction.  
-//! 
 //!
 //! - **H** : The *Half-Carry* flag.  
 //! Set to 1 when an operation results in carrying or borrowing
 //! to bit3.  
-//! 
 //!
 //! - **C** : The *Carry* flag.  
 //! Set to 1 when an operation results in carrying or borrowing to
 //! bit7.  
-//! 
-//!
 //!
 //! The lowest nibble (bit0-3) is always set to 0, even after a load to the 16-bit register `AF`.  
 //!
@@ -32,26 +27,25 @@ use crate::hardware::cpu::Condition;
 use std::ops::{BitAndAssign, BitOrAssign, BitXorAssign};
 
 #[derive(Debug)]
-/// 
+
 pub struct Flags {
     flags: u8,
 }
 
 impl Flags {
-    
-    /// The *Zero* flag.  
+    /// The position of the *Zero* flag in the 8-bit register.  
     /// Set to 1 when the result of an operation is 0; otherwise reset.
     pub const Z: u8 = 0b1000_0000;
 
-    /// The *Subtraction* flag.  
+    /// The position of the *Subtract* flag in the 8-bit register.  
     /// Set to 1 after subtraction instruction.
     pub const N: u8 = 0b0100_0000;
 
-    /// The *Half-Carry* flag.  
+    /// The position of the *Half-Carry* flag in the 8-bit register.  
     /// Set to 1 when an operation results in carrying from or borrowing to bit 3.
     pub const H: u8 = 0b0010_0000;
 
-    /// The *Carry* flag.  
+    /// The position of the *Carry* flag in the 8-bit register.  
     /// Set to 1 when an operation results in carrying from or borrowing to bit 7.
     pub const C: u8 = 0b0001_0000;
 
@@ -92,7 +86,7 @@ impl Flags {
 
     /// Returns true when the flag status matches the condition, otherwise returns false.  
     /// Special case: Condition::Always always returns true.  
-    /// The relation between conditions and flags are as follows: 
+    /// The relation between conditions and flags are as follows:
     ///
     /// | Condition | Flag  |  
     /// |-----------|-------|  
@@ -101,7 +95,7 @@ impl Flags {
     /// |     C     | C = 1 |  
     /// |     Z     | Z = 1 |  
     ///
-    /// 
+    ///
     pub fn check_condition(&self, condition: Condition) -> bool {
         match condition {
             Condition::NC => !self.contains(Flags::C),
@@ -110,5 +104,87 @@ impl Flags {
             Condition::C => self.contains(Flags::C),
             Condition::Always => true,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_toggle() {
+        let mut flags = Flags { flags: 0b0101_0000 };
+        assert_eq!(flags.bits(), 0b0101_0000);
+        flags.toggle(Flags::Z);
+        assert_eq!(flags.bits(), 0b1101_0000);
+        flags.toggle(Flags::Z);
+        assert_eq!(flags.bits(), 0b0101_0000);
+        flags.toggle(Flags::N);
+        assert_eq!(flags.bits(), 0b0001_0000);
+    }
+    #[test]
+    fn test_bits() {
+        let mut flags = Flags { flags: 0b1010_0001 };
+        assert_eq!(flags.bits(), 128 + 32 + 1);
+    }
+
+    #[test]
+    fn test_from_bits_truncate() {
+        let flags = Flags::from_bits_truncate(121);
+        assert_eq!(flags.bits(), 112);
+        let flags = Flags::from_bits_truncate(240);
+        assert_eq!(flags.bits(), 240);
+    }
+
+    #[test]
+    fn test_set() {
+        let mut flags = Flags { flags: 0b1000_0000 };
+
+        assert_eq!(flags.contains(Flags::Z), true);
+        flags.set(Flags::Z, true);
+        assert_eq!(flags.contains(Flags::Z), true);
+
+        flags.set(Flags::C, true);
+        assert_eq!(flags.contains(Flags::C), true);
+        assert_eq!(flags.contains(Flags::H), false);
+        assert_eq!(flags.contains(Flags::N), false);
+
+        flags.set(Flags::Z, false);
+        assert_eq!(flags.contains(Flags::Z), false);
+        assert_eq!(flags.contains(Flags::C), true);
+        assert_eq!(flags.contains(Flags::H), false);
+        assert_eq!(flags.contains(Flags::N), false);
+    }
+
+    #[test]
+    fn test_check_condition() {
+        let flags = Flags { flags: 0b1000_0000 };
+
+        assert_eq!(flags.check_condition(Condition::NC), true);
+        assert_eq!(flags.check_condition(Condition::NZ), false);
+        assert_eq!(flags.check_condition(Condition::Z), true);
+        assert_eq!(flags.check_condition(Condition::C), false);
+        assert_eq!(flags.check_condition(Condition::Always), true);
+    }
+
+    #[test]
+    fn test_contains() {
+        let flags = Flags { flags: 0b1010_0000 };
+
+        assert_eq!(flags.contains(Flags::Z), true);
+        assert_eq!(flags.contains(Flags::C), false);
+
+        assert_eq!(flags.contains(Flags::C), false);
+        assert_eq!(flags.contains(Flags::H), true);
+    }
+
+    #[test]
+    fn test_empty() {
+        let flags = Flags::empty();
+
+        assert_eq!(flags.contains(Flags::Z), false);
+        assert_eq!(flags.contains(Flags::C), false);
+        assert_eq!(flags.contains(Flags::C), false);
+        assert_eq!(flags.contains(Flags::H), false);
     }
 }
