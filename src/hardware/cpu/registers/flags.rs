fd7baa10e5ec880fc9284ff705800ad56d3cd030
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-04-05 11:56:13 +0200

    Added doc

--- src/hardware/cpu/registers/flags.rs
+++ src/hardware/cpu/registers/flags.rs
@@ -1,27 +1,61 @@
+//! `Flag Register` F.  
+//!
+//! The `Flag Register` is a 8-bit register consisting in 4 flags (`Z`, `N`, `H`, and `C`) that are set or reset according to the result of instruction execution.  
+//!
+//! | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//! |---|---|---|---|---|---|---|---|
+//! | Z | N | H | C | 0 | 0 | 0 | 0 |
+//!
+//! - **Z** : The *Zero* flag.  
+//! Set to 1 when the result of an operation is 0; otherwise reset.  
+//!
+//! 
+//! - **N** : The *Subtract* flag.  
+//! Set to 1 after a subtraction instruction.  
+//! 
+//!
+//! - **H** : The *Half-Carry* flag.  
+//! Set to 1 when an operation results in carrying or borrowing
+//! to bit3.  
+//! 
+//!
+//! - **C** : The *Carry* flag.  
+//! Set to 1 when an operation results in carrying or borrowing to
+//! bit7.  
+//! 
+//!
+//!
+//! The lowest nibble (bit0-3) is always set to 0, even after a load to the 16-bit register `AF`.  
+//!
+
 use crate::hardware::cpu::Condition;
 use std::ops::{BitAndAssign, BitOrAssign, BitXorAssign};
 
-/// Consists of 4 flags that are set and reset according to the results of instruction
-/// execution
 #[derive(Debug)]
+/// 
 pub struct Flags {
     flags: u8,
 }
 
 impl Flags {
-    /// Set to 1 when the result of an operation is 0; otherwise reset
+    
+    /// The *Zero* flag.  
+    /// Set to 1 when the result of an operation is 0; otherwise reset.
     pub const Z: u8 = 0b1000_0000;
 
-    /// Set to 1 following execution of the subtraction instruction, regardless of the result
+    /// The *Subtraction* flag.  
+    /// Set to 1 after subtraction instruction.
     pub const N: u8 = 0b0100_0000;
 
-    /// Set to 1 when an operation results in carrying from or borrowing to bit 3
+    /// The *Half-Carry* flag.  
+    /// Set to 1 when an operation results in carrying from or borrowing to bit 3.
     pub const H: u8 = 0b0010_0000;
 
-    /// Set to 1 when an operation results in carrying from or borrowing to bit 7
+    /// The *Carry* flag.  
+    /// Set to 1 when an operation results in carrying from or borrowing to bit 7.
     pub const C: u8 = 0b0001_0000;
 
-    /// Returns true if *flag* is set in the `Flag Register`.  
+    /// Returns true if *flag* is set in the `Flag Register`.
     pub fn contains(&self, flag: u8) -> bool {
         (self.flags & flag) == flag
     }
@@ -34,7 +68,7 @@ impl Flags {
         }
     }
 
-    /// Creates an empty `Flag Register`.  
+    /// Constructs an empty `Flag Register`.  
     pub fn empty() -> Self {
         Self { flags: 0 }
     }
@@ -58,13 +92,16 @@ impl Flags {
 
     /// Returns true when the flag status matches the condition, otherwise returns false.  
     /// Special case: Condition::Always always returns true.  
-    /// The relation between conditions and flags are as follows:  
-    ///  | Condition | Flag  |  
-    ///  | --------- | ----- |  
-    ///  |     NC    | C = 0 |  
-    ///  |     NZ    | Z = 0 |  
-    ///  |     C     | C = 1 |  
-    ///  |     Z     | Z = 1 |  
+    /// The relation between conditions and flags are as follows: 
+    ///
+    /// | Condition | Flag  |  
+    /// |-----------|-------|  
+    /// |     NC    | C = 0 |  
+    /// |     NZ    | Z = 0 |  
+    /// |     C     | C = 1 |  
+    /// |     Z     | Z = 1 |  
+    ///
+    /// 
     pub fn check_condition(&self, condition: Condition) -> bool {
         match condition {
             Condition::NC => !self.contains(Flags::C),
